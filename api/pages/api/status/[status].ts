16a602d60d4f5e951834738d0352ae40332d17cd
iShawnWang <iShawnWang2333@gmail.com>
2022-10-02 14:00:31 +0800

    feat: cors

--- api/pages/api/status/[status].ts
+++ api/pages/api/status/[status].ts
@@ -1,14 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
+
+const cors = Cors()
 
 type Data = {
   status: string
 }
 
-export default function handler(
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await runMiddleware(req, res, cors)
+
   const { status } = req.query
 
   res.status(Number(status)).json({ status: status?.toString() || '' })
